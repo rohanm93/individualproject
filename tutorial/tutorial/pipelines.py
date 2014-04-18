10874a6ad76af89c50d49c4f4b86a4e39d9ccab2
Rohan Mahtani <rohan.mahtani@gmail.com>
2014-04-18 06:08:12 +0100

    added ordering of columns

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,15 +5,15 @@
 
 class TutorialPipeline(object):
     def process_item(self, item, spider):
-    	splitted = item['p1FirstServePercentage'].split('%')[0]
-		item['p1FirstServePercentage'] = splitted
-		item['p1FirstServePointsWonPercentage'] = item['p1FirstServePointsWonPercentage'].split('%')[0]
-		item['p1SecondServePointsWon'] = item['p1SecondServePointsWon'].split('%')[0]
-		item['p1FirstReturnPointsWon'] = item['p1FirstReturnPointsWon'].split('%')[0]
-		item['p1SecondReturnPointsWon'] = item['p1SecondServePointsWon'].split('%')[0]
-		item['p1BreakPointsWon'] = item['p1BreakPointsWon'].split('%')[0]
-		item['p1AverageFirstServeSpeed'] = item['p1AverageFirstServeSpeed'].split('k')[0]
-		item['p1AverageSecondServeSpeed'] = item['p1AverageSecondServeSpeed'].split('k')[0]
+       	splitted = item['p1FirstServePercentage'].split('%')[0]
+	item['p1FirstServePercentage'] = splitted
+	item['p1FirstServePointsWonPercentage'] = item['p1FirstServePointsWonPercentage'].split('%')[0]
+	item['p1SecondServePointsWon'] = item['p1SecondServePointsWon'].split('%')[0]
+	item['p1FirstReturnPointsWon'] = item['p1FirstReturnPointsWon'].split('%')[0]
+	item['p1SecondReturnPointsWon'] = item['p1SecondServePointsWon'].split('%')[0]
+	item['p1BreakPointsWon'] = item['p1BreakPointsWon'].split('%')[0]
+	item['p1AverageFirstServeSpeed'] = item['p1AverageFirstServeSpeed'].split('k')[0]
+	item['p1AverageSecondServeSpeed'] = item['p1AverageSecondServeSpeed'].split('k')[0]
         item['p2FirstServePercentage'] = item['p2FirstServePercentage'].split('%')[0]
         item['p2FirstServePointsWonPercentage'] = item['p2FirstServePointsWonPercentage'].split('%')[0]
         item['p2SecondServePointsWon'] = item['p2SecondServePointsWon'].split('%')[0]
@@ -22,5 +22,5 @@ class TutorialPipeline(object):
         item['p2BreakPointsWon'] = item['p2BreakPointsWon'].split('%')[0]
         item['p2AverageFirstServeSpeed'] = item['p2AverageFirstServeSpeed'].split('k')[0]
         item['p2AverageSecondServeSpeed'] = item['p2AverageSecondServeSpeed'].split('k')[0]
-		item['duration'] = item['duration'].split(' ')[0]
-		return item
+	item['duration'] = item['duration'].split(' ')[0]
+	return item
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -12,6 +12,50 @@ SPIDER_MODULES = ['tutorial.spiders']
 NEWSPIDER_MODULE = 'tutorial.spiders'
 ITEM_PIPELINES = {'tutorial.pipelines.TutorialPipeline' : 100}
 
+FEED_EXPORTERS = {
+    'csv': 'tutorial.feedexport.CSVkwItemExporter'
+}
+
+# By specifying the fields to export, the CSV export honors the order
+# rather than using a random order.
+EXPORT_FIELDS = [
+    'matchId',
+    'tournament',
+    'player1',
+    'player2',
+    'winner',
+    't_round',
+    'duration'
+    'p1Aces',
+    'p1DoubleFaults',
+    'p1FirstServePercentage',
+    'p1FirstServePointsWonPercentage',
+    'p1SecondServePointsWon',
+    'p1FirstReturnPointsWon',
+    'p1SecondReturnPointsWon',
+    'p1BreakPointsWon',
+    'p1Winners',
+    'p1UnforcedErrors',
+    'p1NetApproaches',
+    'p1PointsWonAtNet',
+    'p1AverageFirstServeSpeed',
+    'p1AverageSecondServeSpeed',
+    'p2Aces',
+    'p2DoubleFaults',
+    'p2FirstServePercentage',
+    'p2FirstServePointsWonPercentage',
+    'p2SecondServePointsWon',
+    'p2FirstReturnPointsWon',
+    'p2SecondReturnPointsWon',
+    'p2BreakPointsWon',
+    'p2Winners',
+    'p2UnforcedErrors',
+    'p2NetApproaches',
+    'p2PointsWonAtNet',
+    'p2AverageFirstServeSpeed',
+    'p2AverageSecondServeSpeed',
+    'urlVisited'
+]
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
