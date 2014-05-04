1b2c03085af63537684a598e8c42a1b74808c7a7
Rohan M <rm2611@imperial.ac.uk>
2014-05-04 23:07:11 +0100

    part of the difference in points equation implemented

--- process_clusters/process_clusters.py
+++ process_clusters/process_clusters.py
@@ -2,10 +2,13 @@
 
 import csv
 
-
+input_file = csv.DictReader(open("federer_clusters.csv"))
+# notation/formula implemented:
+# ABi =(spw(A,Ci)-(1-rpw(A,Ci))) - (spw(B,Ci)-(1-rpw(B,Ci)))
+# ABi = Difference(A,Ci)         - Difference(B,Ci)
+difference_a_c = 0
 
 def find_clusters_of_player(player_name):
-	input_file = csv.DictReader(open("federer_clusters.csv"))
 	list_of_clusters = []
 	for row in input_file:
 		#player2 = str(row["player2"])
@@ -17,7 +20,22 @@ def find_clusters_of_player(player_name):
 		# break
 	print list_of_clusters
 
-find_clusters_of_player("Novak Djokovic")
+def find_players_in_cluster(cluster_name):
+	global difference_a_c
+	for row in input_file:
+		cluster = row["cluster"]
+		if cluster_name == cluster:
+			player = row["player2"]
+			swp_p2 = float(row["p2PointWonOnServe"])
+			rwp_p2 = float(row["p2ReturnPointWonPercentage"])
+			#manipulating p2 values to p1 & converting to decimals
+			#below is the same as spw(A,Ci)-(1-rpw(A,Ci))
+			difference_spw_rpw_p1 = (100.0-rwp_p2-swp_p2)/100
+			print player + ", " + cluster + ", " + str(difference_spw_rpw_p1)
+			difference_a_c += difference_spw_rpw_p1
+
+#find_clusters_of_player("Novak Djokovic")
+find_players_in_cluster("cluster_1")
 
 
 '''	with open('federer_clusters.csv', 'rb') as f:
@@ -32,5 +50,5 @@ find_clusters_of_player("Novak Djokovic")
 
         	for field in row:
         		if field==player_name:
-        			#opponent player found
+        			#opponent player foundr
 '''
