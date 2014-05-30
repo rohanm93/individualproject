ea557dce2ae7f25bf74c59de7f3c2786e35303d6
Rohan M <rm2611@imperial.ac.uk>
2014-05-30 17:52:01 +0100

    code to sort dataset by date. sorted dataset added

--- process_dataset/process_dataset.py
+++ process_dataset/process_dataset.py
@@ -1,8 +1,9 @@
 # this code creates a player's profile
 from __future__ import division
+from datetime import datetime
 import csv
 
-input_file = csv.DictReader(open("berdych_stats.csv"))
+#input_file = csv.DictReader(open("k-means/almagro.csv"))
 
 #finds average net approaches and average first serve
 def get_info_serve_net(player_name):
@@ -45,5 +46,16 @@ def get_info_serve_net(player_name):
 		print total_net_approaches/count_net
 	print total_serve_speed/count_serve
 
-get_info_serve_net("Berdych")
+
+def sort_dataset_by_date():
+	reader = csv.reader(open("k-means/youzhny.csv"), delimiter=",")
+	headers = reader.next()
+	data = sorted(reader, key = lambda row: datetime.strptime(row[6], "%b %d %Y"))
+	with open("k-means_datesorted/youzhny.csv", "wb") as f:
+		writer = csv.writer(f, delimiter=',')
+		writer.writerow(headers)
+		writer.writerows(data)
+
+sort_dataset_by_date()
+#get_info_serve_net("Berdych")
 
