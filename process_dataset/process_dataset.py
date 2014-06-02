334d894c3d173dd029fcc26ae4a9c2373fc5470a
Rohan M <rm2611@imperial.ac.uk>
2014-06-02 16:55:18 +0100

    1) redid clustering with fewer outliers. added new dataset 2)wrote code to get fixtures of all games

--- process_dataset/process_dataset.py
+++ process_dataset/process_dataset.py
@@ -46,16 +46,36 @@ def get_info_serve_net(player_name):
 		print total_net_approaches/count_net
 	print total_serve_speed/count_serve
 
-
+#';f  cd'
 def sort_dataset_by_date():
-	reader = csv.reader(open("k-means/youzhny.csv"), delimiter=",")
-	headers = reader.next()
-	data = sorted(reader, key = lambda row: datetime.strptime(row[6], "%b %d %Y"))
-	with open("k-means_datesorted/youzhny.csv", "wb") as f:
+	reader1 = csv.reader(open("outliers_fixed/almagro.csv"), delimiter=",")
+	headers = reader1.next()
+	reader = csv.reader(open("fixtures.csv"), delimiter=",")
+	data = sorted(reader, key = lambda row: datetime.strptime(row[5], "%b %d %Y"))
+	with open("fixtures-datesorted.csv", "wb") as f:
 		writer = csv.writer(f, delimiter=',')
 		writer.writerow(headers)
 		writer.writerows(data)
 
+def get_fixtures_to_predict():
+	players_list = ["Almagro", "Kevin Anderson", "Berdych", "Jeremy Chardy", "Cilic", "Potro", "Grigor Dimitrov", "Djokovic", "Federer", "David Ferrer", "Gasquet", "Gulbis", "Haas", "Hewitt", "Isner", "Monfils", "Nadal", "Nishikori", "Raonic", "Robredo", "Seppi", "Tsonga", "Verdasco", "Wawrinka", "Youzhny"]
+	filepath_list = ["almagro", "anderson", "berdych", "chardy", "cilic", "delpotro", "dimitrov", "djokovic", "federer", "ferrer", "gasquet", "gulbis", "haas", "hewitt", "isner", "monfils", "nadal", "nishikori", "raonic", "robredo", "seppi", "tsonga", "verdasco", "wawrinka", "youzhny"]
+	filepath_string = "outliers_fixed/"
+	all_fixtures = []
+	output = open("fixtures.csv", "wb")
+	writer = csv.writer(output, delimiter=',')
+	for fp in filepath_list:
+		reader = csv.reader(open(filepath_string+fp+".csv"), delimiter=',')
+		headers = reader.next()
+		for rec in reader:
+			if any(x in rec[2] for x in players_list):
+				writer.writerow(rec)
+
+
+		#data = list(list(rec) for rec in reader)
+
+
+#get_fixtures_to_predict()
 sort_dataset_by_date()
 #get_info_serve_net("Berdych")
 
